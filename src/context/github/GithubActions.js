f324e059774ae8029356118f5bd864d88020c624
mahammajid12 <i160186@nu.edu.pk>
2023-07-30 01:31:15 +0500

    updated

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,27 +1,31 @@
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+//const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 // search users
 export const searchUsers = async (text) => {
     const params = new URLSearchParams({
         q: text,
     })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`,{
-        headers: {
-            Authorization: `token ${GITHUB_TOKEN}`,
-        },
-    })
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`,
+    // {
+    //     headers: {
+    //         Authorization: `token ${GITHUB_TOKEN}`,
+    //     },
+    // }
+    )
     const {items} = await response.json()
     return items
 }
 
 // Get single user
 export const getUser = async (login) => {
-    const response = await fetch(`${GITHUB_URL}/users/${login}`,{
-        headers: {
-            Authorization: `token ${GITHUB_TOKEN}`,
-        },
-    })
+    const response = await fetch(`${GITHUB_URL}/users/${login}`,
+    // {
+    //     headers: {
+    //         Authorization: `token ${GITHUB_TOKEN}`,
+    //     },
+    // }
+    )
     if (response.status === 404){
         window.location = '/notfound'
     }
@@ -37,11 +41,13 @@ export const getUserRepos = async (login) => {
         sort: 'created',
         per_page: 10
     })
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,{
-        headers: {
-            Authorization: `token ${GITHUB_TOKEN}`,
-        },
-    })
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
+    // {
+    //     headers: {
+    //         Authorization: `token ${GITHUB_TOKEN}`,
+    //     },
+    // }
+    )
     const data = await response.json()
     return data
 }
