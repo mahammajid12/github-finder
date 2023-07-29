2654a59a07edb59156fbf10872cc06f3bcb1abf6
mahammajid12 <i160186@nu.edu.pk>
2023-07-30 01:24:28 +0500

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import UserSearch from '../components/users/UserSearch'
 function Home() {
   return (
     <>
-        <h2 className='mb-8'>This website is used to find github users, their profile and reposisotories info.</h2>
+        <h2 className='mb-8'>This website is used to find github users, their profile and reposisotories info...</h2>
         <UserSearch />
         <UserResults />
         {/* <h1>Welcomee</h1> */}
